bb3850d4deb370018bf3234b5bc3bfa2f58a6f91
Danilo Pereira da Silva <ra233377@frajola.lab.ic.unicamp.br>
2024-05-11 17:03:20 -0300

    Código funcionando

---
+++ projeto1/files/bgnet_source/examples/servidor_musicas.c
@@ -0,0 +1,68 @@
+/*
+** server.c -- a stream socket server demo
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <sys/wait.h>
+#include <signal.h>
+
+
+#define PORT "3490"  // the port users will be connecting to
+
+#define BACKLOG 10	 // how many pending connections queue will hold
+
+void sigchld_handler(int s)
+{
+	(void)s; // quiet unused variable warning
+
+	// waitpid() might overwrite errno, so we save and restore it:
+	int saved_errno = errno;
+
+	while(waitpid(-1, NULL, WNOHANG) > 0);
+
+	errno = saved_errno;
+}
+
+
+// get sockaddr, IPv4 or IPv6:
+void *get_in_addr(struct sockaddr *sa)
+{
+	if (sa->sa_family == AF_INET) {
+		return &(((struct sockaddr_in*)sa)->sin_addr);
+	}
+
+	return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
+
+int main(void)
+{
+	struct addrinfo hints, *res;
+	int sockfd;
+
+	// first, load up address structs with getaddrinfo():
+
+	memset(&hints, 0, sizeof hints);
+	hints.ai_family = AF_UNSPEC;     // AF_INET, AF_INET6, or AF_UNSPEC
+	hints.ai_socktype = SOCK_STREAM; // SOCK_STREAM or SOCK_DGRAM
+
+	getaddrinfo("www.example.com", "3490", &hints, &res);
+
+	// make a socket using the information gleaned from getaddrinfo():
+	sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+
+	bind(sockfd, res->ai_addr, res->ai_addrlen);
+
+	listen(sockfd, 10);
+
+	return 0;
+}
+
