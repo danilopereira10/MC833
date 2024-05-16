cc7e9b70ff75cf5f3c37f464bf1d2e04ece9a3cb
Danilo Pereira da SIlva <danilopereira_b2@hotmail.com.br>
2024-05-16 17:59:04 -0400

    _

--- projeto1/files/bgnet_source/examples/cliente_musicas.c
+++ projeto1/files/bgnet_source/examples/cliente_musicas.c
@@ -44,7 +44,7 @@ void *get_in_addr(struct sockaddr *sa)
 }
 
 #define MYPORT "3490"
-int main(void)
+int main(int argc, char* argv[])
 {
 	struct addrinfo hints, *res;
     int sockfd;
@@ -62,22 +62,7 @@ int main(void)
 
 
 	connect(sockfd, res->ai_addr, res->ai_addrlen);
-struct addrinfo hints, *res;
-    int sockfd;
-
-    // first, load up address structs with getaddrinfo():
-
-    memset(&hints, 0, sizeof hints);
-    hints.ai_family = AF_UNSPEC;     // AF_INET, AF_INET6, or AF_UNSPEC
-    hints.ai_socktype = SOCK_STREAM; // SOCK_STREAM or SOCK_DGRAM
-
-    getaddrinfo("www.example.com", "3490", &hints, &res);
-
-    // make a socket using the information gleaned from getaddrinfo():
-    sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
-
-
-	connect(sockfd, res->ai_addr, res->ai_addrlen);
+	
 	int op = 0;
 	while (op < 1 && op > 8) {
 		printf("Selecione a operação: \n");
@@ -91,15 +76,30 @@ struct addrinfo hints, *res;
 		printf("8 - fazer um download de uma música a partir de seu identificador \n");
 		scanf("%d", &op);
 	}
-	
-	switch (op) {
-		case 6:
-			int temp = htonl(op);
-			send(sockfd, &temp, sizeof temp, 0);
-			break;
-		case 7:
-			break;
+	int op = atoi(argv[1]); 
+	int i = 0;
+	char buffer[1024];
+	buffer[0] = '0' + op;
+	if ((op!= 7)) {
+		char* content = argv[2];
+		while (content[i] != '\0') {
+			buffer[i+1] = content[i];
+			i++;
+		}
 	}
+	i++;
+	buffer[i] = '\0';
+	int total = 0;
+	int bytesleft = i + 1, len = i + 1;
+	int n;
+	while (total < len) {
+		n  = send(sockfd, buffer+total, bytesleft, 0);
+		if (n == -1) { break; }
+		total += n;
+		bytesleft -= n;
+	}
+	
+
 	
 	
 	
