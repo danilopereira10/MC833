f46c7f42522bad98ee01d795e4c426afb42db01c
Danilo Pereira da SIlva <danilopereira_b2@hotmail.com.br>
2024-06-18 15:21:02 -0400

    Estado atual

--- projeto3/script.py
+++ projeto3/script.py
@@ -2,13 +2,16 @@ import nest_asyncio
 nest_asyncio.apply()
 from scapy.all import *
 
-def get_icmp_packets(packets, srcip, destip):
+#Método para obter os pacotes ICMP de um determinado host para outro
+def get_icmp_packets(packets, sourceIp, destinationIp):
     icmp_packets = [packet for packet in packets if ICMP in packet]
-    packetsFromSource = [packet for packet in icmp_packets if packet[IP].src == srcip]
-    packetsFromSourceToDestination = [packet for packet in packetsFromSource if packet[IP].dst == destip]
-
+    packetsFromSource = [packet for packet in icmp_packets if packet[IP].src == sourceIp]
+    packetsFromSourceToDestination = [packet for packet in packetsFromSource if packet[IP].dst == destinationIp]
+    packetsFromSourceToDestination[0].show()
+    packetsFromSourceToDestination[1].show()
     return packetsFromSourceToDestination
 
+#Método para calcular a quantidade de bytes presente numa lista de pacotes, desconsiderando o primeiro pacote
 def amountOfBytes(packets):
     totalBytes = 0
     for index in range(1, len(packets)):
@@ -16,6 +19,7 @@ def amountOfBytes(packets):
     return totalBytes
 
 
+#Método para imprimir as informações desejadas
 def print_info(packets, sourceIp, destinationIp):
     packets = get_icmp_packets(packets, sourceIp, destinationIp)
     print("src: ", sourceIp)
@@ -31,7 +35,7 @@ def print_info(packets, sourceIp, destinationIp):
 
 
 if __name__=='__main__':
-    pcap_file = "c.pcap"
+    pcap_file = "cap.pcap"
     packets = rdpcap(pcap_file)
     print_info(packets, sourceIp="10.0.0.1", destinationIp="10.0.0.3")
     print_info(packets, sourceIp="10.0.0.3", destinationIp="10.0.0.1")
