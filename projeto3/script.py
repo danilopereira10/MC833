9d2c2c22f957c13bc4dc8f03711f966e740fdc9d
Danilo Pereira da SIlva <danilopereira_b2@hotmail.com.br>
2024-06-18 16:10:57 -0400

    Mudanças no código

--- projeto3/script.py
+++ projeto3/script.py
@@ -7,8 +7,7 @@ def get_icmp_packets(packets, sourceIp, destinationIp):
     icmp_packets = [packet for packet in packets if ICMP in packet]
     packetsFromSource = [packet for packet in icmp_packets if packet[IP].src == sourceIp]
     packetsFromSourceToDestination = [packet for packet in packetsFromSource if packet[IP].dst == destinationIp]
-    packetsFromSourceToDestination[0].show()
-    packetsFromSourceToDestination[1].show()
+
     return packetsFromSourceToDestination
 
 #Método para calcular a quantidade de bytes presente numa lista de pacotes, desconsiderando o primeiro pacote
@@ -20,24 +19,26 @@ def amountOfBytes(packets):
 
 
 #Método para imprimir as informações desejadas
-def print_info(packets, sourceIp, destinationIp):
-    packets = get_icmp_packets(packets, sourceIp, destinationIp)
+def print_info(allPackets, sourceIp, destinationIp):
+    sentPackets = get_icmp_packets(allPackets, sourceIp, destinationIp)
     print("src: ", sourceIp)
     print("dest: ", destinationIp)
+
+    receivedPackets = get_icmp_packets(allPackets, sourceIp=destinationIp, destinationIp=sourceIp)
     
-    highestArrivalTime = packets[-1].time
-    totalBytesSent = amountOfBytes(packets)
-    print("Throughput:", (totalBytesSent) / (highestArrivalTime-packets[0].time), "bytes per second")
-    averageIntervalDifference = (highestArrivalTime-packets[0].time) / (len(packets)-1)
-    print("Intervalo médio entre os pacotes:", averageIntervalDifference, "segundos")
-    print("Total de pacotes:", len(packets))
+    highestArrivalTime = sentPackets[-1].time
+    totalBytesSent = amountOfBytes(sentPackets)
+    print("Throughput:", totalBytesSent / (highestArrivalTime-sentPackets[0].time), "bytes por segundo")
+    averageIntervalDifference = (highestArrivalTime-receivedPackets[0].time) / (len(sentPackets)-1)
+    print("Intervalo médio De chegada de pacotes:", averageIntervalDifference, "segundos")
+    print("Total de pacotes enviados:", len(sentPackets))
     print()
 
 
 if __name__=='__main__':
-    pcap_file = "capt.pcap"
-    packets = rdpcap(pcap_file)
-    print_info(packets, sourceIp="10.0.0.1", destinationIp="10.0.0.3")
-    print_info(packets, sourceIp="10.0.0.3", destinationIp="10.0.0.1")
-    print_info(packets, sourceIp="10.0.0.2", destinationIp="10.0.0.4")
-    print_info(packets, sourceIp="10.0.0.4", destinationIp="10.0.0.2")
+    pcap_file1 = "cap1.pcap"
+    pcap_file2 = "cap2.pcap"
+    packets1 = rdpcap(pcap_file1)
+    packets2 = rdpcap(pcap_file2)
+    print_info(packets1, sourceIp="10.0.0.1", destinationIp="10.0.0.3")
+    print_info(packets2, sourceIp="10.0.0.2", destinationIp="10.0.0.4")
